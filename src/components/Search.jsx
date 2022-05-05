856c69b60d4ec52661c3320fea672f60efff389e
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-05-05 23:35:01 +0530

    Create API request using axios

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
+import ImageResults from './ImageResults';
 
 class Search extends Component {
     state = {
@@ -13,10 +15,21 @@ class Search extends Component {
     };
 
     onTextChange = (e) => {
-        this.setState({ searchText: e.target.value });
+        this.setState({ [e.target.name]: e.target.value },
+            () => {
+                axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+                    .then(res => this.setState({ images: res.data.hits }))
+                    .catch(err => console.log(err));
+            });
+    }
+
+    onAmountChange = (e, index, value) => {
+        this.setState({ amount: value });
+
     }
 
     render() {
+        console.log(this.state.images);
         return (
             <div>
                 <TextField
